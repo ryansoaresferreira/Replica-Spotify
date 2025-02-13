8b6b2ba098a9479d6aa899bfc588269ab90c7e9b
ryansoaresferreira <rs8081519@gmail.com>
2025-02-13 11:25:59 -0300

    refactoring all code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import logoSpotify from "../assets/logo/spotify-logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <img src={logoSpotify} alt="Logo do Spotify" />
-      
-      <a className="header__link" href="/">
-       
+      <Link to="/">
+        <img src={logoSpotify} alt="Logo do Spotify" />
+      </Link>
+
+      <Link to="/" className="header__link">
         <h1>Spotify</h1>
-      </a>
+      </Link>
     </div>
   );
 };
---
+++ src/pages/home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Main from "../components/Main";
+
+const home = () => {
+  return <Main/>
+};
+
+export default home;
